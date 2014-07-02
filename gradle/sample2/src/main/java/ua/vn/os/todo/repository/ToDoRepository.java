8b37b7aebd7bcbd63ed6efe8b5ba814d4fcb0ae3
oleg_sukhov <oleg.sukhov@email.ua>
2014-07-02 22:00:32 +0300

    Add <ToDo> project to repository

---
+++ gradle/sample2/src/main/java/ua/vn/os/todo/repository/ToDoRepository.java
@@ -0,0 +1,13 @@
+package ua.vn.os.todo.repository;
+
+import ua.vn.os.todo.model.ToDoItem;
+import java.util.Collection;
+import java.util.List;
+
+public interface ToDoRepository {
+	List<ToDoItem> findAll();
+	ToDoItem findById(Long id);
+	Long insert(ToDoItem toDoItem);
+	void update(ToDoItem toDoItem);
+	void delete(ToDoItem toDoItem);
+}
