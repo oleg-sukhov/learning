1d2edcc7e806b7279e579d0c8b2e5346b50b44f0
oleg_sukhov <oleg.sukhov@email.ua>
2014-07-08 22:30:54 +0300

    Make external task <part2>

--- gradle/sample2/buildSrc/src/main/groovy/ua/vn/os/tasks/ReleaseVersionTask.groovy
+++ gradle/sample2/buildSrc/src/main/groovy/ua/vn/os/tasks/ReleaseVersionTask.groovy
@@ -1,6 +1,9 @@
 package ua.vn.os.tasks
 
-import org.gradleApi.*
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
 
 class ReleaseVersionTask extends DefaultTask {
 	@Input Boolean release
